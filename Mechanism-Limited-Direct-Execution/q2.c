eaf394255921c685e7bae550a4d4633c5d8444cc
李明發 <134141397+MingFaTW@users.noreply.github.com>
2024-10-17 03:16:04 +0800

    modify the context switch problem @@

--- Mechanism-Limited-Direct-Execution/q2.c
+++ Mechanism-Limited-Direct-Execution/q2.c
@@ -9,58 +9,66 @@
 #define stringSize 16
 #define toNarrowSeconds 1000000000
 
-long getTimeInterval(struct timespec start, struct timespec end) {
-    return (end.tv_sec - start.tv_sec) * toNarrowSeconds + (end.tv_nsec - start.tv_nsec);
+long averageReadWriteTime, averageProcessTime;
+
+void measure_time_readAndWrite() {
+    int pipe_fd[2];
+    pipe(pipe_fd);
+    struct timespec start, end;
+    char pipeWrite[stringSize] = "mingfaisgoodman"; 
+    char pipeRead[stringSize] = "mingfaisgoodman";
+    long readAndWriteTime = 0;
+    clock_gettime(CLOCK_MONOTONIC, &start);  
+    for (int i = 0; i < iterationsForSystemCall; i++) {
+        write(pipe_fd[1], pipeWrite, stringSize); 
+        read(pipe_fd[0], pipeRead, stringSize); 
+    }
+    clock_gettime(CLOCK_MONOTONIC, &end);  
+    
+    readAndWriteTime = (end.tv_sec - start.tv_sec) * toNarrowSeconds + (end.tv_nsec - start.tv_nsec);
+    close(pipe_fd[0]);
+    close(pipe_fd[1]);    
+    averageReadWriteTime = readAndWriteTime / (iterationsForSystemCall);
+    printf("Read&Write average time: %ld ns\n", averageReadWriteTime); 
 }
 
 void measure_time_context_switch() {
     int pipe_fd[2];
     pipe(pipe_fd);
-
-    struct timespec start, end, readStart, readEnd, writeStart, writeEnd;
+    struct timespec start, end;
     int pid = fork();
     if (pid < 0) {
         perror("fork failed");
         exit(1);
     }
-    
     char dataForParent[stringSize] = "mingfaisgoodman"; 
-    char dataForChild[stringSize];  
-    long readAndWriteTime = 0;
-
+    char dataForChild[stringSize] = "mingfaisgoodman";  
     if (pid == 0) {
         for (int i = 0; i < iterationsForSystemCall; i++) {
-            clock_gettime(CLOCK_MONOTONIC, &readStart);  
             read(pipe_fd[0], dataForChild, stringSize); 
-            clock_gettime(CLOCK_MONOTONIC, &readEnd);  
-            
-            clock_gettime(CLOCK_MONOTONIC, &writeStart);  
             write(pipe_fd[1], dataForChild, stringSize); 
-            clock_gettime(CLOCK_MONOTONIC, &writeEnd);  
-
-            readAndWriteTime += getTimeInterval(readStart, readEnd) + getTimeInterval(writeStart, writeEnd);
         }
-        close(pipe_fd[0]);
-        close(pipe_fd[1]);
         exit(0);
     } else { 
         clock_gettime(CLOCK_MONOTONIC, &start);  
         for (int i = 0; i < iterationsForSystemCall; i++) {
             write(pipe_fd[1], dataForParent, stringSize);  
-            read(pipe_fd[0], dataForParent, stringSize);   
+            read(pipe_fd[0], dataForParent, stringSize);  
         }
         clock_gettime(CLOCK_MONOTONIC, &end);  
 
         wait(NULL); 
         close(pipe_fd[0]);
         close(pipe_fd[1]);
-        
-        long totalContextSwitchTime = getTimeInterval(start, end) - readAndWriteTime;
-        printf("Context switch average time: %ld ns\n", totalContextSwitchTime / (iterationsForSystemCall * 2)); 
-    }
+        long ProcessTime = ( (end.tv_sec - start.tv_sec) * toNarrowSeconds + (end.tv_nsec - start.tv_nsec) );
+        averageProcessTime = ProcessTime / (iterationsForSystemCall * 2);
+        printf("Process average time: %ld ns\n", averageProcessTime); 
+    } 
 }
 
 int main() {
+    measure_time_readAndWrite();
     measure_time_context_switch();
+    printf("Context switch average time: %ld ns\n", averageProcessTime - averageReadWriteTime);
     return 0;
 }
